0b00cd5f4cc2eae77070f2b6d327e164f9f4048d
abdulrahman <develoandroijava@gmail.com>
2021-01-28 21:01:41 +0300

    new virsion

--- lib/ui/screens/homePage.dart
+++ lib/ui/screens/homePage.dart
@@ -4,10 +4,9 @@ class homePage extends StatefulWidget {
   @override
   _homePageState createState() => _homePageState();
 }
-//mnbvcx
+
 class _homePageState extends State<homePage> {
-  bool test;
-  String name;
+
   @override
   Widget build(BuildContext context) {
     return Container(
